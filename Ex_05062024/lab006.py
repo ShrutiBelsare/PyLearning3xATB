59adf3485dc6c74c70910a1a71dc81580f3c2f89
shruti17 <shrutibelsare17@gmail.com>
2024-06-06 21:13:02 +0530

    adding lab006.py file

---
+++ Ex_05062024/lab006.py
@@ -0,0 +1,18 @@
+# data type
+age = 45
+print(type(age))
+isMale = False
+print(type(isMale))
+name = "shruti"
+print(type(name))
+pi=3.147
+print(type(pi))
+complex_number = 2+3j
+print(complex_number.imag)
+print(complex_number.real)
+
+# run time change
+age = 34
+print(type(age))
+age = "thirty four"
+print(type(age))
