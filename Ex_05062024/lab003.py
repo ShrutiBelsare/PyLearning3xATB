0774e40698bbfc35ca8e841a3da5cd5a220e98ba
shruti17 <shrutibelsare17@gmail.com>
2024-06-06 19:19:21 +0530

    adding lab003.py file

---
+++ Ex_05062024/lab003.py
@@ -0,0 +1,14 @@
+# Print command
+# different arguments in the print command
+print("hello", "world", 123, 3.14, True)
+# seperator by default has single space
+print("hello", "world", 123, 3.14, True, sep="")
+print("hello", "world", 123, 3.14, True, sep="   ")
+print("hello", "world", 123, 3.14, True, sep="_")
+print("hello", "world", 123, 3.14, True, sep="---")
+# 1 argument
+print("Hi, My name is Shruti")
+# 2 args
+print("Hi, My name is Shruti", "Belsare", sep="-")
+
+
