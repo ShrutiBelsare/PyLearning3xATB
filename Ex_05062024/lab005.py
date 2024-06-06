3358a7ff1776f9ccd28a41c5fc7d920f6bf352e5
shruti17 <shrutibelsare17@gmail.com>
2024-06-06 20:28:08 +0530

    adding lab005.py file

---
+++ Ex_05062024/lab005.py
@@ -0,0 +1,6 @@
+# find max of numbers
+print(max(100,23,67,11,99000354))
+print(max(-2,-4,-1000))
+# following gives error--NameError: name 'shruti' is not defined
+# print(max(100,23,67,11,29990000,54,22,6,shruti))
+print(max(100,23,67,11,4655990000,54,22,6,7.8))
