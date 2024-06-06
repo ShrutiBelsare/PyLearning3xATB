33886959e2eb5c3b5650df6866099a129e4d033a
shruti17 <shrutibelsare17@gmail.com>
2024-06-06 19:29:28 +0530

    adding lab004.py file

---
+++ Ex_05062024/lab004.py
@@ -0,0 +1,13 @@
+# new line gets added
+print("Hi, Shruti", "Belsare")
+print("Hi, Shruti", "Belsare")
+# clubbed together in 1 line
+print("Hi, Shruti", "Belsare", end="")
+print("Hi, Shruti", "Belsare")
+# tab
+print("Hi, Shruti", "Belsare", end="\t")
+print("Hi, Shruti", "Belsare")
+# tab and seperator
+print("Hi, Shruti", "Belsare", sep="-", end="\t")
+print("Hi, Shruti", "Belsare")
+
